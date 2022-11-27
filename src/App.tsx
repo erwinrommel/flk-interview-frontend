7cb877ed1d0c51aa22c11621385948492ba88bb4
Erwin <erwin@acoba.io>
2022-11-28 06:29:01 +1100

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@
 //   2. Add number, date, and multiline input fields to the existing form
 //   3. Create an identical form with controlled state
 
-import { Form } from "react-final-form";
-import { TextField } from "./Input";
+import { Form, Field } from "react-final-form";
+import { InputField, DateInput, MultiLineInput, NumberInput, TextInput } from './components'
 import "./App.css";
 
 function App() {
@@ -14,7 +14,23 @@ function App() {
     <Form onSubmit={values => window.alert(JSON.stringify(values))}>
       {({ handleSubmit }) => (
         <form onSubmit={handleSubmit} className="form">
-          <TextField label="Text field" name="textField" />
+          <InputField label="Text field">
+            <Field name="textField" component={TextInput}/>
+          </InputField>
+          <InputField label="Number field">
+            <Field name="numberField" component={NumberInput}/>
+            </InputField>
+          <InputField label="Date field">
+            <Field name="dateField" component={DateInput}/>
+          </InputField>
+          <InputField label="MultiLine field">
+            <Field
+              name="multiLineField"
+              render={(props) => (
+                <MultiLineInput {...props} rows={5}/>
+              )}
+            />
+          </InputField>
           <button type="submit">Submit</button>
         </form>
       )}
---
+++ src/components/MultiLineInput/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FieldRenderProps } from "react-final-form";
+import "./MultiLineInput.css";
+
+type Props = FieldRenderProps<string, any>;
+
+const MultiLineInput: React.FC<Props> = ({ input, meta, ...rest }: Props) => {
+  console.log(rest, input)
+  return (
+  <textarea {...input} {...rest} />
+)
+};
+
+export { MultiLineInput };
